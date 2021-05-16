4512c8cf1bc7a3d1d2087bd1e9509c093706d020
ThunderboltSH <74146327+ThunderboltSH@users.noreply.github.com>
2021-05-16 18:26:55 +0400

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,4 +1,5 @@
 #Youtube Downloader GUI
+#ThunderboltSH
 #Limitations - no validation for invalid URLs but a fix will be coming soon
 #Program will be reuploaded with more comments later
 from tkinter import *
